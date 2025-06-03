f0231c44c84e0a410fed4dbf64a5c997758f1eab
KZelab <69273221+KZelab@users.noreply.github.com>
2025-06-03 11:36:23 +0100

    Exercise 3 - displaying a sequence of numbers

---
+++ DisplaySequenceOfNumbers.py
@@ -0,0 +1,16 @@
+# Exercise 3 Display the following sequence of numbers:
+#
+# 0 1 2 3 4 5 6 7 8 9 10 .... 44
+
+i = 0
+
+exitcondition = 44
+
+while True:
+    print(i)
+    if i == exitcondition:
+        break
+    i = i + 1
+    # i += 1 fancy way
+    # i++ i--
+
