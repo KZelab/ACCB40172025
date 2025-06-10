e2e8c500d2fee7b2e51694dca49550140b858b16
KZelab <69273221+KZelab@users.noreply.github.com>
2025-06-10 16:30:02 +0100

    Exercise 5b two inputs draw a rectangle

---
+++ HipHipArray.py
@@ -0,0 +1,21 @@
+
+
+myList = []
+
+myList.append(7)
+
+myList.append(10)
+
+myList.append(4)
+
+# [ 7, 10, 4 ]
+
+
+print(f"my list values {myList} and the length of the list is {len(myList)}")
+
+print(len("the wet farts came with lumps"))
+
+
+print(f"the popped off value {myList.pop()}" )
+
+print("the altered list", myList)
